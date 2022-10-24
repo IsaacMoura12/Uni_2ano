346a86210cb8c9eb0393a1b8a806fd8d8f767bb2
Isaac Moura <isaac.moura@ua.pt>
2022-10-24 18:18:19 +0100

    Add files via upload

---
+++ Prog_Orientada_Objetos/Aula1/Hello.java
@@ -0,0 +1,10 @@
+package Aula1;
+
+public class Hello {
+        public static void main(String[] args) throws Exception {
+            System.out.println("Hello, World!");
+         
+        }
+    
+    
+}
